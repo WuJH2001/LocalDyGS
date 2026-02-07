81069d47d5c34f939daa3dfe067be5d2c0577272
AAAma <58819382+WuJH2001@users.noreply.github.com>
2026-02-07 17:31:00 +0800

    Update colmap_loader.py

--- scene/colmap_loader.py
+++ scene/colmap_loader.py
@@ -253,12 +253,11 @@ def read_intrinsics_binary(path_to_model_file):
     return cameras
 
 
-
 def read_extrinsics_text(path):
     """
     Taken from https://github.com/colmap/colmap/blob/dev/scripts/python/read_write_model.py
     """
-    images = {}
+    images = []
     with open(path, "r") as fid:
         while True:
             line = fid.readline()
@@ -276,11 +275,13 @@ def read_extrinsics_text(path):
                 xys = np.column_stack([tuple(map(float, elems[0::3])),
                                        tuple(map(float, elems[1::3]))])
                 point3D_ids = np.array(tuple(map(int, elems[2::3])))
-                images[image_id] = Image(
+                images.append(Image(
                     id=image_id, qvec=qvec, tvec=tvec,
                     camera_id=camera_id, name=image_name,
-                    xys=xys, point3D_ids=point3D_ids)
+                    xys=xys, point3D_ids=point3D_ids))
+    images.sort(key=sort_by_image_name)
     return images
+
 
 
 def read_colmap_bin_array(path):
